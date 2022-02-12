3e131a5b8b4b708cf74451e946122e076150358e
Andy <andyalvarez@correo.unimet.edu.ve>
2022-02-12 10:31:52 -0400

    Carrusel funcionando

---
+++ main.js
@@ -0,0 +1,20 @@
+window.addEventListener('load',function(){
+   var img=[]
+   img[0]="images/1.jpg";
+   img[1]="images/2.jpg";
+   img[2]="images/3.jpg"
+
+   var n=0;
+
+   function Slide(){
+       document.slide.src = img[n];
+
+       if(n<2){
+           n++;
+       }
+       else{
+           n=0;
+       }
+   }
+   setInterval(Slide,6000);
+});
