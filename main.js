1fa26907ebe6b8fe90d1404601e0b91e73e9ad80
Andy <andyalvarez@correo.unimet.edu.ve>
2022-02-12 15:54:28 -0400

    que loco

--- main.js
+++ main.js
@@ -18,3 +18,17 @@ window.addEventListener('load',function(){
    }
    setInterval(Slide,6000);
 });
+
+document.getElementById("Enviar").onclick = function(){
+    x=true
+    x1=true
+    x2=true
+    const el1=document.getElementById("formula").value;
+    const el2=document.getElementById("formula2").value;
+    const el3=document.getElementById("mensaje").value;
+    document.getElementById("formula").value !== "" ? el1:x=false;
+    document.getElementById("formula2").value !== "" ? el2:x1=false;
+    document.getElementById("mensaje").value !== "" ? el3:x2=false;
+    x === true&& x1 === true && x2 === true ? alert("se ha enviado el mensaje"): alert("Introduzca los datos correctamente por favor")
+    x === true&& x1 === true && x2 === true ? console.log(`Nombre: ${el1} correo: ${el2} mensaje:${el3}`): console.log("se introdujeron datos incorrectos");
+}
